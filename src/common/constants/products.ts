ac1c3170386f68978fad7f347e560cf10d2646d6
Jesus Gastelum <gastelumtec21@gmail.com>
2024-02-19 21:59:17 -0700

    add temporal products images

---
+++ src/common/constants/products.ts
@@ -0,0 +1,28 @@
+export const TEST_PRODUCTS = [
+    {
+        name: 'Pantalla Smart TV',
+        price: 4544,
+        img: '/img/pantalla.jpg'
+    },
+    {
+        name: 'Pantalla Smart TV',
+        price: 4544,
+        img: '/img/pantalla2.webp'
+    },
+    {
+        name: 'Pantalla Smart TV',
+        price: 4544,
+        img: '/img/pantalla3.webp'
+    },
+    {
+        name: 'Pantalla Smart TV',
+        price: 4544,
+        img: '/img/pantalla.jpg'
+    },
+
+    {
+        name: 'Pantalla Smart TV',
+        price: 4544,
+        img: '/img/pantalla.jpg'
+    },
+]
