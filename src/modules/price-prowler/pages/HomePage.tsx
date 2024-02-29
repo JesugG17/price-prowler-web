cd12bdd0da03a2a1f8bc2f97da31c045536ec4b6
Jesus Gastelum <gastelumtec21@gmail.com>
2024-02-28 22:45:52 -0700

    add provisional homepage

--- src/modules/price-prowler/pages/HomePage.tsx
+++ src/modules/price-prowler/pages/HomePage.tsx
@@ -1,3 +1,16 @@
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='w-full px-4 h-screen flex flex-col justify-center items-center'>
+      <p className='text-3xl opacity-70 text-center'>
+        Busca tus productos en un solo <br />
+        <strong>CLICK</strong>
+      </p>
+      <img className='w-40' src='/img/homepage.jpg' alt='' />
+      <button className='bg-black rounded text-white py-1 px-4 shadow-lg hover:brightness-110 transition-all duration-200'>
+        Comenzar
+      </button>
+      <div className='bg-slate-200 w-40 h-40 opacity-30 rounded-full absolute -top-12 -right-12'></div>
+      <div className='bg-slate-200 w-40 h-40 opacity-30 rounded-full absolute -bottom-12 -left-12'></div>
+    </div>
+  );
 };
