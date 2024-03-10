b80cff1c7ca7900152343beab6f245d0ce4510b2
Monica <angulosmonica@gmail.com>
2024-03-10 14:48:05 -0700

    cambio de TrackingPage de tailwind a scss

--- src/common/routes/AppRouter.tsx
+++ src/common/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router';
 import { HomePage } from '../../modules/price-prowler/pages/HomePage/HomePage';
-import { TrackingPage } from '../../modules/price-prowler/pages/TrackingPage';
+import { TrackingPage } from '../../modules/price-prowler/pages/TrackingPage/TrackingPage';
 import { SearchPage } from '@/modules/price-prowler/pages/SearchPage/SearchPage';
 import { Header } from '../../modules/ui/components/Header/Header';
 
--- src/modules/price-prowler/pages/TrackingPage/TrackingPage.tsx
+++ src/modules/price-prowler/pages/TrackingPage/TrackingPage.tsx
@@ -1,21 +1,22 @@
 import { useProducts } from '@/common/context/price-prowler/ProductsAddedProvider';
-import { ProductCard } from '../components/ProductCard';
+import { ProductCard } from '../../components/ProductCard';
 import { useNavigate } from 'react-router';
+import './TrackingPage.scss';
 
 export const TrackingPage = () => {
   const { products, addProduct, removeProduct } = useProducts();
   const navigate = useNavigate();
   return (
     <>
-      <div className='flex p-2'>
-        <button onClick={() => navigate('/search')} className='px-3 bg-black text-white rounded'>
+      <div className='header-tracking-page'>
+        <button onClick={() => navigate('/search')} className=''>
           Back
         </button>
-        <h1 className='text-2xl font-bold flex-1 text-center'>Tracking page</h1>
+        <h1>Tracking page</h1>
       </div>
       <hr />
 
-      <ul className='flex flex-wrap gap-5 justify-center '>
+      <ul className='list-tracking'>
         {products.map((product) => (
           <ProductCard
             key={product.id}
