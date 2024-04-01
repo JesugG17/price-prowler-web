3a3898ef9c173eff4fbd946bef30cd83f16d3476
Jesus Gastelum <gastelumtec21@gmail.com>
2024-04-01 13:05:01 -0700

    Remove default tailwind configs and use sass configs instead

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ProductsAddedProvider } from './common/context/price-prowler/ProductsAddedProvider';
+
+import './global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/modules/ui/components/Header/Header.tsx
+++ src/modules/ui/components/Header/Header.tsx
@@ -1,15 +1,7 @@
-import { useState } from 'react';
-import { Sidebar } from '../Sidebar';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
-  const [openMobileMenu, setOpenMobileMenu] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setOpenMobileMenu((prevState) => !prevState);
-  };
-
   return (
     <header>
       <nav>
@@ -24,12 +16,11 @@ export const Header = () => {
             <Link to='/tracking'>Monitoreo</Link>
           </li>
         </ul>
-        <img onClick={toggleMobileMenu} className='md:hidden' src='/img/menu-icon.png' alt='' />
+        {/* <img onClick={toggleMobileMenu} className='md:hidden' src='/img/menu-icon.png' alt='' /> */}
         <div className='container-nav-button'>
           <button>Iniciar sesion</button>
           <button>Registrarse</button>
         </div>
-        <Sidebar isMobileMenuOpen={openMobileMenu} toggleMobileMenu={toggleMobileMenu} />
       </nav>
     </header>
   );
