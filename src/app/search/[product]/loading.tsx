b11d2c809be05d3f4bafc3a8856e688b6189c9af
Jesus Gastelum <gastelumtec21@gmail.com>
2024-04-27 21:54:33 -0700

    Render a fallback ui when SC is fetching

---
+++ src/app/search/[product]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className="w-full h-screen bg-black bg-opacity-20 absolute top-0 right-0 z-20 flex-center text-white">CARGANDO...</div>;
+}
--- src/app/search/[product]/page.tsx
+++ src/app/search/[product]/page.tsx
@@ -1,6 +1,7 @@
 import { ProductsList } from '@/components/products/products-list';
 import { Product } from '@/interfaces/product.interface';
 import { Suspense } from 'react';
+import Loading from './loading';
 
 const getProducts = async (productName: string): Promise<Product[]> => {
   const resp = await fetch(`http://localhost:3002/api/search/products/${productName}`);
@@ -11,7 +12,7 @@ const getProducts = async (productName: string): Promise<Product[]> => {
 export default async function Search({ params }: Props) {
   const products = await getProducts(params.product);
   return (
-    <Suspense fallback={<div className="w-full h-screen bg-black bg-opacity-25 absolute top-0 right-0">LOADING</div>}>
+    <Suspense fallback={<Loading />}>
       <div>
         <h1>Search page</h1>
         <ProductsList products={products} />
